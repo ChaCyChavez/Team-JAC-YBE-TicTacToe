fcd90ef0da4d236b7f48fe5634274d43f3d0c749
[ChaCyChavez] <[cschavez@up.edu.ph]>
2016-04-08 15:36:21 +0800

    Edit MainPanel and GamePanel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -16,10 +16,12 @@ public class GamePanel {
 
 	public GamePanel(){
 		mainPanel = new JPanel();
+		JPanel leftPanel = new JPanel();
+		JPanel rightPanel = new JPanel();
 		topPanel = new JPanel();
 		centerPanel = new JPanel();
 		bottomPanel = new JPanel();
-		mainPanel.setLayout(new BorderLayout());
+		mainPanel.setLayout(new BorderLayout(100, 0));
 		topPanel.setLayout(new FlowLayout());
 		centerPanel.setLayout(new GridLayout(3,3));
 		bottomPanel.setLayout(new GridLayout(1,3));
@@ -46,9 +48,12 @@ public class GamePanel {
 		centerPanel.add(seven);
 		centerPanel.add(eight);
 		centerPanel.add(nine);
+		centerPanel.setPreferredSize(new Dimension(50, 50));
 		bottomPanel.add(player1);
 		bottomPanel.add(game);
 		bottomPanel.add(player2);
+		mainPanel.add(leftPanel, BorderLayout.LINE_START);
+		mainPanel.add(rightPanel, BorderLayout.LINE_END);
 		mainPanel.add(topPanel, BorderLayout.PAGE_START);
 		mainPanel.add(centerPanel, BorderLayout.CENTER);
 		mainPanel.add(bottomPanel, BorderLayout.PAGE_END);
--- src/MenuPanel.java
+++ src/MenuPanel.java
@@ -4,12 +4,14 @@ import java.awt.event.*;
 
 public class MenuPanel{
 	private JPanel mainPanel;
+	private JPanel upperPanel;
 	private JPanel centerPanel;
 	private JPanel lowerPanel;
 	private JPanel player1Panel;
 	private JPanel player2Panel;
 	private JPanel bestOfPanel;
 	private JPanel okayPanel;
+	private JLabel welcomeLb;
 	private JLabel player1;
 	private JLabel player2;
 	private JLabel bestOf;
@@ -19,9 +21,10 @@ public class MenuPanel{
 	private JButton okay;
 	private JButton cancel;
 
-	
+
 	public MenuPanel(){
 		mainPanel = new JPanel();
+		upperPanel = new JPanel();
 		centerPanel = new JPanel();
 		lowerPanel = new JPanel();
 		player1Panel = new JPanel();
@@ -29,23 +32,39 @@ public class MenuPanel{
 		bestOfPanel = new JPanel();
 		okayPanel = new JPanel();
 		mainPanel.setLayout(new BorderLayout());
-		centerPanel.setLayout(new FlowLayout());
+		centerPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 10));
 		lowerPanel.setLayout(new FlowLayout());
+
+		welcomeLb = new JLabel("Welcome to Tic-Tac-Toe!");
+		welcomeLb.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 16));
+
 		player1 = new JLabel("Player 1 Name: ");
-		name1 = new JTextField(15);
+		name1 = new JTextField(25);
+		name1.setHorizontalAlignment(JTextField.CENTER);
+        name1.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 16));
+        name1.setMargin(new Insets(2, 2, 2, 2));
+
 		player2 = new JLabel("Player 2 Name: ");
-		name2 = new JTextField(15);
-		bestOf = new JLabel("Best of: ");
-		rounds= new JTextField(15);
+		name2 = new JTextField(25);
+		name2.setHorizontalAlignment(JTextField.CENTER);
+        name2.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 16));
+        name2.setMargin(new Insets(2, 2, 2, 2));
+
+		bestOf = new JLabel("Best of (Should be odd no.): ");
+		rounds= new JTextField(20);
+		rounds.setHorizontalAlignment(JTextField.CENTER);
+        rounds.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 16));
+        rounds.setMargin(new Insets(2, 2, 2, 2));
+
 		okay = new JButton("OK");
         cancel = new JButton("Cancel");
-        
-		player1Panel.add(player1);
-		player1Panel.add(name1);
-		player2Panel.add(player2);
-		player2Panel.add(name2);
-		bestOfPanel.add(bestOf);
-		bestOfPanel.add(rounds);
+
+		// player1Panel.add(player1);
+		// player1Panel.add(name1);
+		// player2Panel.add(player2);
+		// player2Panel.add(name2);
+		// bestOfPanel.add(bestOf);
+		// bestOfPanel.add(rounds);
 		okayPanel.add(okay);
 		okayPanel.add(cancel);
 
@@ -55,10 +74,19 @@ public class MenuPanel{
                 System.exit(1);
             }
 		});
-		centerPanel.add(player1Panel);
-		centerPanel.add(player2Panel);
-		centerPanel.add(bestOfPanel);
+		// centerPanel.add(player1Panel);
+		// centerPanel.add(player2Panel);
+		// centerPanel.add(bestOfPanel);
+		upperPanel.add(welcomeLb);
+		centerPanel.add(player1);
+		centerPanel.add(name1);
+		centerPanel.add(player2);
+		centerPanel.add(name2);
+		centerPanel.add(bestOf);
+		centerPanel.add(rounds);
 		lowerPanel.add(okayPanel);
+
+		mainPanel.add(upperPanel, BorderLayout.PAGE_START);
 		mainPanel.add(centerPanel, BorderLayout.CENTER);
 		mainPanel.add(lowerPanel, BorderLayout.PAGE_END);
 	}
